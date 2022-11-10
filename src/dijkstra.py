5014079be3b0055fe00603a141bc886a6ee151c0
Henri Immonen <henri.immonen000@gmail.com>
2022-11-10 21:31:52 +0200

    dijkstran algoritmi toimii

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,19 +1,30 @@
-import heapq
+from collections import deque
 
 class Dijkstra():
-    def __init__(self, alku, loppu, kartta):
+    def __init__(self, alku, loppu, verkko, alustettu_kartta):
         self.alku = alku
         self.loppu = loppu
-        self.kartta = kartta
-        self.keko = []
-        self.kasitellyt = []
+        self.verkko = verkko
+        self.jono = deque([])
+        self.kartta = alustettu_kartta
+        self.kasitellyt = set({})
+        self.etaisyys = [[] for i in range(len(self.kartta[0])*len(self.kartta))]
+        self.edeltava = [-1 for i in range(len(self.kartta[0])*len(self.kartta))]
 
     def etsi_reitti(self):
-        return 
-        heapq.heappush(self.keko, self.alku)
-        while not self.keko.empty():
-            solmu = heapq.heappop(self.keko)
-            if self.kasitellyt[solmu]:
-                continue
-            self.kasitellyt[solmu] = True
-            kaaret = self.solmun_kaaret(solmu)
+        self.jono.append(self.alku)
+        self.kasitellyt.add(self.alku)
+        self.etaisyys[self.alku[0]*len(self.kartta[0])+self.alku[1]] = 0
+        while self.jono:
+            solmu = self.jono.popleft()
+            for naapuri in self.verkko[(solmu[0]*len(self.kartta[0])+solmu[1])]:
+
+                if naapuri in self.kasitellyt:
+                    continue
+
+                self.jono.append(naapuri)
+                self.kasitellyt.add(naapuri)
+                self.etaisyys[naapuri[0]*len(self.kartta[0])+naapuri[1]] = (self.etaisyys[solmu[0]*len(self.kartta[0])+solmu[1]]+1)
+                self.edeltava[naapuri[0]*len(self.kartta[0])+naapuri[1]] = (solmu[0]*len(self.kartta[0])+solmu[1])
+        return self.edeltava
+
--- src/index.py
+++ src/index.py
@@ -2,7 +2,6 @@ from dijkstra import Dijkstra
 import time
 
 class Main():
-
     def kartan_alustaminen(self):
             f = open('./test.map', 'r')
             kartta = []
@@ -16,27 +15,25 @@ class Main():
         self.verkko = [] # Luodaan verkko
         for i in range(len(self.kartta)):
             for j in range(len(self.kartta[i])):
-                self.tarkista_naapurit((i,j))
+                self._tarkista_naapurit((i,j))
 
-    def tarkista_naapurit(self, solmu):
+    def _tarkista_naapurit(self, solmu):
         solmun_naapurit = [] # Jokaisella solmulla on lista naapureista
         for naapuri in [(solmu[0]-1,solmu[1]-1), (solmu[0]-1, solmu[1]), (solmu[0]-1,solmu[1]+1),
                             (solmu[0],solmu[1]-1),(solmu[0],solmu[1]+1),(solmu[0]+1,solmu[1]-1),
                             (solmu[0]+1,solmu[1]),(solmu[0]+1,solmu[1]+1)]:
             if self._verkon_sisalla(naapuri):
-                #print('Solmu on verkon sisällä', naapuri)
                 if self._solmun_naapuriin_voi_kulkea(naapuri):
-                    #print('Solmun tarkistus')
                     solmun_naapurit.append(naapuri)
         self.verkko.append(solmun_naapurit)
 
-    def _verkon_sisalla(self, solmu):
+    def _verkon_sisalla(self, solmu): # Tarkistetaan onko seuraava solmu matriisin sisällä
         if solmu[0] >= 0 and solmu[0] < len(self.kartta):
             if solmu[1] >= 0 and solmu[1] < len(self.kartta[0]):
                 return True
         return False
 
-    def _solmun_naapuriin_voi_kulkea(self, naapuri):
+    def _solmun_naapuriin_voi_kulkea(self, naapuri): # Tarkistetaan onko seuraavaan ruutuun mahdollista kulkea
         if self.kartta[naapuri[0]][naapuri[1]] == '.':
             return True
         return False
@@ -44,15 +41,24 @@ class Main():
     def main(self):
         self.kartan_alustaminen()
         alku = (0,0)
-        loppu = (-1,-1)
-        alku = time.time()
+        loppu = (5,13)
+        alku_aika = time.time()
         self.kaarilista()
-        print('Aikaa kului: ', time.time()- alku, 'sekuntia')
+        print('Aikaa kului: ', time.time()- alku_aika, 'sekuntia')
         print('Kaarilista tehty!')
-        print(self.verkko)
-        #print(self.verkko[(0,0)])
-        #d = Dijkstra(alku, loppu, alustettu_kartta)
-        #d.etsi_reitti()
+        d = Dijkstra(alku, loppu, self.verkko, self.kartta)
+        etaisyys = d.etsi_reitti()
+
+        path = []
+        edeltava = loppu[0]*len(self.kartta[0])+loppu[1]
+        path.append(edeltava)
+
+        while etaisyys[edeltava] != -1:
+            path.append(etaisyys[edeltava])
+            edeltava = etaisyys[edeltava]
+        print("Polku ruudusta:", alku, 'ruutuun', loppu, ': ')
+        for i in range(len(path)-1, -1, -1):
+            print((path[i]//len(self.kartta[0]), path[i]%len(self.kartta[0])))
 
 
 if __name__ == '__main__':
