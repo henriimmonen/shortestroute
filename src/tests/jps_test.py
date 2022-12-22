721e36bd29e4e3475fd959fef0d80d74b470a45e
Henri Immonen <henri.immonen000@gmail.com>
2022-12-22 18:53:58 +0200

    Lisätty testejä

--- src/tests/jps_test.py
+++ src/tests/jps_test.py
@@ -6,8 +6,8 @@ from jps import JumpPointSearch
 class TestRoute(unittest.TestCase):
     def setUp(self):
         self.kartta = ['...', '.@@', '...']
-        self.kartta_hyppysolmu = ['..@..', '..@..', '.@...', '.....']
-        self.kartta_hyppysolmu_oikea_vasen = ['..@', '...', '...']
+        self.kartta_hyppysolmu_horisontaalinen = ['..@..', '..@..', '.@...', '.@...']
+        self.kartta_hyppysolmu_vertikaalinen = ['..@', '@..', '...']
         self.verkko = [[(0, 1), (1, 0)], [(0, 0), (0, 2)], [(0, 1)], [(0, 0), (2, 0), (2, 1)], [],
                        [], [(1, 0), (2, 1)], [(2, 0), (2, 2)], [(2, 1)]]
         self.jps = JumpPointSearch((0, 0), (2, 2), self.verkko, self.kartta)
@@ -40,11 +40,21 @@ class TestRoute(unittest.TestCase):
         self.assertEqual((2, 2), self.jps.etsi_diagonaalisesti((2, 2), -1, -1))
 
     def test_etsi_horisontaalisesti_tunnistaa_hyppysolmun_ylaoikea(self):
-        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu)
-        jps_hyppysolmu_ylaoikea = JumpPointSearch((0, 0), (3, 4), verkko, self.kartta_hyppysolmu)
-        self.assertEqual((2, 2), jps_hyppysolmu_ylaoikea.etsi_horisontaalisesti((2, 2), 0, -1))
+        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu_horisontaalinen)
+        jps_hyppysolmu = JumpPointSearch((0, 0), (3, 4), verkko, self.kartta_hyppysolmu_horisontaalinen)
+        self.assertEqual((2, 2), jps_hyppysolmu.etsi_horisontaalisesti((2, 2), 0, -1))
 
     def test_etsi_horisontaalisesti_tunnistaa_hyppysolmun_alaoikea(self):
-        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu)
-        jps_hyppysolmu_alaoikea = JumpPointSearch((0, 0), (3, 4), verkko, self.kartta_hyppysolmu)
-        self.assertEqual((1, 1), jps_hyppysolmu_alaoikea.etsi_horisontaalisesti((1, 1), 0, 1))
+        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu_horisontaalinen)
+        jps_hyppysolmu = JumpPointSearch((0, 0), (3, 4), verkko, self.kartta_hyppysolmu_horisontaalinen)
+        self.assertEqual((1, 1), jps_hyppysolmu.etsi_horisontaalisesti((1, 1), 0, 1))
+
+    def test_etsi_vertikaalisesti_tunnistaa_hyppysolmun_alaoikea(self):
+        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu_vertikaalinen)
+        jps_hyppysolmu = JumpPointSearch((0, 0), (2, 2), verkko, self.kartta_hyppysolmu_vertikaalinen)
+        self.assertEqual((0, 1), jps_hyppysolmu.etsi_vertikaalisesti((0, 1), 1, 0))
+
+    def test_etsi_vertikaalisesti_tunnistaa_hyppysolmun_alavasen(self):
+        verkko = self.apufunktiot.kaarilista(self.kartta_hyppysolmu_vertikaalinen)
+        jps_hyppysolmu = JumpPointSearch((0, 0), (2, 2), verkko, self.kartta_hyppysolmu_vertikaalinen)
+        self.assertEqual((1, 1), jps_hyppysolmu.etsi_vertikaalisesti((1, 1), 1, 0))
