a368b4b5b087b4bff7aefdf1d8ca5f20a52014d9
Henri Immonen <henri.immonen000@gmail.com>
2022-12-23 15:05:40 +0200

    Algoritmeja verrataan ajallisesti

--- src/algoritmien_vertailu.py
+++ src/algoritmien_vertailu.py
@@ -4,15 +4,21 @@ from jps import JumpPointSearch
 import apufunktiot
 
 def main():
+    """Ohjelman tarkoitus on vertailla
+    Dijkstran ja Jps-algoritmin toimintaa"""
+
     algoritmien_reitin_pituusvertailu()
+    algoritmien_aikavertailu()
 
 def algoritmien_reitin_pituusvertailu():
+    """Metodi algoritmien tuottamien reittien pituusvertailuun"""
+
     alku = time.time()
     kartta = apufunktiot.alusta_kartta('./sydney.map')
     aloitus_solmu = (0, 0)
     lopetus_solmu = (255, 255)
 
-    for ajo in range(101):
+    for ajo in range(100):
         if ajo == 10:
             aloitus_solmu = (255, 255)
             lopetus_solmu = (0, 0)
@@ -66,8 +72,64 @@ def algoritmien_reitin_pituusvertailu():
             break
         print(ajo, aloitus_solmu, lopetus_solmu)
     loppuaika = time.time()
-    print(loppuaika-alku, 's')
+    print(loppuaika-alku, 'sekuntia')
     print('Kuljetut matkat vastaavat molemmilla algoritmeilla toisiaan')
+
+def algoritmien_aikavertailu():
+    """Vertaillaan Dijkstran ja JPS-algoritmin suorituskykyä"""
+    kartta = apufunktiot.alusta_kartta('./sydney.map')
+    aloitus_solmu = (0, 0)
+    lopetus_solmu = (255, 255)
+    aika_dijkstra, aika_jps = aikavertailun_suoritus(
+        aloitus_solmu, lopetus_solmu, kartta)
+    print('Dijkstran keskiarvo vasemmasta yläkulmasta oikeaan alakulmaan',
+    aika_dijkstra)
+    print('Jps:n keskiarvo vasemmasta yläkulmasta oikeaan alakulmaan',
+    aika_jps)
+
+    aloitus_solmu = (0, 0)
+    lopetus_solmu = (0, 255)
+    aika_dijkstra, aika_jps = aikavertailun_suoritus(
+        aloitus_solmu, lopetus_solmu, kartta)
+    print('Dijkstran keskiarvo vasemmasta yläkulmasta oikeaan yläkulmaan',
+    aika_dijkstra)
+    print('Jps:n keskiarvo vasemmasta yläkulmasta oikeaan yläkulmaan',
+    aika_jps)
+
+    aloitus_solmu = (0, 0)
+    lopetus_solmu = (140, 255)
+    aika_dijkstra, aika_jps = aikavertailun_suoritus(
+        aloitus_solmu, lopetus_solmu, kartta)
+    print('Dijkstran keskiarvo vasemmasta yläkulmasta oikeaan puoliväliin',
+    aika_dijkstra)
+    print('Jps:n keskiarvo vasemmasta yläkulmasta oikeaan puoliväliin',
+    aika_jps)
+
+    aloitus_solmu = (0, 0)
+    lopetus_solmu = (14, 50)
+    aika_dijkstra, aika_jps = aikavertailun_suoritus(
+        aloitus_solmu, lopetus_solmu, kartta)
+    print('Dijkstran keskiarvo lyhyellä matkalla', aika_dijkstra)
+    print('Jps:n keskiarvo lyhyellä matkalla', aika_jps)
+
+
+def aikavertailun_suoritus(aloitus_solmu, lopetus_solmu, kartta):
+    """Moduuli, jossa mitataan ajan keskiarvo 100 matkalle annettujen solmujen välillä"""
+    aloitus_aika_dijkstra = time.time()
+    for ajo in range(100): # pylint: disable=unused-variable
+        verkko = apufunktiot.kaarilista(kartta)
+        dijkstra = Dijkstra(aloitus_solmu, lopetus_solmu, verkko, kartta)
+        dijkstra.etsi_reitti()
+    aika_dijkstra = (time.time() - aloitus_aika_dijkstra)/100
+
+    aloitus_aika_jps = time.time()
+    for ajo in range(100):
+        verkko = apufunktiot.kaarilista(kartta)
+        jps = JumpPointSearch(aloitus_solmu, lopetus_solmu, verkko, kartta)
+        jps.hae_reitti()
+    aika_jps = (time.time() - aloitus_aika_jps)/100
+
+    return aika_dijkstra, aika_jps
 
 if __name__ == '__main__':
     main()
